40ae461967dfa5c0169c26eb3a0c8fc5520073a9
Vitor Paraizo <103430661+vitorparaizo@users.noreply.github.com>
2025-02-26 00:28:31 +0000

    repetição

---
+++ .vscode/aula25_02/Exemplo04For.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Exemplo04For {
+  public static Scanner input = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    int num1, num2, qtdInterv;
+
+    System.out.print("Digite um número inteiro: ");
+    num1 = input.nextInt();
+
+    do {
+      System.out.print("Digite um número inteiro maior que " + num1 + ": ");
+      num2 = input.nextInt();
+    } while (num2 <= num1);
+
+    qtdInterv = num2 - num1 + 1;
+    System.out.println(
+      "Existem " + qtdInterv + " números no intervalo de " +
+      num1 + " a " + num2
+    );
+    System.out.print("E esses números são: { " + num1);
+
+    for (int i = num1; i <= num2; ++i) {
+      System.out.print(", " + i);
+    }
+    System.out.println(" }");
+    
+  }
+}
--- hello-word-aula/aula_java_21_02/Aula03.java
+++ hello-word-aula/aula_java_21_02/Aula03.java
@@ -1,9 +1,10 @@
+package aula_java_21_02;
 import java.util.Scanner;
 public class Aula03 {
 
     public static Scanner input = new Scanner(System.in);
 
-    public static void maiin (String[] args){
+    public static void main (String[] args){
         int num;
         System.out.print("Digite um numero inteiro: ");
         num = input.nextInt();
@@ -17,7 +18,23 @@ public class Aula03 {
                 System.out.println("o numero " + num + " neutro");
             } else{
                 System.out.print("o numero " + num + "não é negativo");
-            }  
+            }
+        
+            double altura;
+            double peso; 
+
+            System.out.print("Sua altura: ");
+            altura = input.nextDouble();
+
+            System.out.print("seu peso");
+            peso = input.nextDouble();
+
+            if (peso / (altura * altura) >= 40.0) {
+                System.out.print("you're fat! ");
+            } else {
+                System.out.print("you the next best americans next top model");
+            }
+
         }
     }
 }
